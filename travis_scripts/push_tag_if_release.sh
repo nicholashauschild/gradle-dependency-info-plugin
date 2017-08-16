2252990bd528379e886497f52cb2e0077c197be9
Nick Hauschild <nicholas.hauschild@gmail.com>
2017-08-16 00:24:48 -0500

    Providing travis build access to write to github (for tagging)

--- travis_scripts/push_tag_if_release.sh
+++ travis_scripts/push_tag_if_release.sh
@@ -5,6 +5,9 @@ echo "Read version [$version] from version.txt"
 
 if ! [[ $version =~ ^.*SNAPSHOT$ ]];
 then
+    git config --global user.email "builds@travis-ci.com"
+    git config --global user.name "Travis CI"
+
     echo "Pushing tag to remote v$version"
-    git push origin v$version
+    git push -q https://$githubToken@github.com/$TRAVIS_REPO_SLUG v$version
 fi
